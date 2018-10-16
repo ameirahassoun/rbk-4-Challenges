13338f1c6994f842bb07a17c5c6a17b134100fd6
Ameira-Hassoun <afhassoun5@gmail.com>
2018-10-16 18:19:11 +0300

    Common Character

---
+++ CommonCharachters.js
@@ -0,0 +1,33 @@
+// Common Characters
+// Write a function that accepts two strings as arguments, and returns only the characters that are common to both strings.
+
+// Your function should return the common characters in the same order that they appear in the first argument. Do not return duplicate characters and ignore whitespace in your returned string.
+
+// Example: commonCharacters('acexivou', 'aegihobu')
+
+// Returns: 'aeiou'
+
+
+commonCharacters = function(string1, string2){
+  
+  var arr =[];
+  
+   for(var i=0; i<string1.length; i++){
+     
+     for(var k=0; k<string2.length; k++){
+       
+       if(string1[i] === string2[k]){
+         
+         if (string1[i] !== " " && arr.indexOf(string1[i]) === -1)
+         
+         arr.push(string1[i])
+         
+       }
+       
+     }
+     
+   }
+   
+   return arr.join("")
+   
+};
